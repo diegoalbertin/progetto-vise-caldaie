d4a4fee7355e6b7d7df200a1072bc59fe8596b9d
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2023-04-28 08:37:13 +0200

    iniziata grafica pagina home

--- pages/calcolaBollette.php
+++ pages/calcolaBollette.php
@@ -1,5 +1,5 @@
 <?php 
-require_once dirname(__FILE__)."\interfacce\createClassesAndInterfaces.php";
+require_once dirname(__FILE__)."..\..\interfacce\createClassesAndInterfaces.php";
 
 
 function calcolaBollette($consumoKw,$consumoSMC){
